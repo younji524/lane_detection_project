9ea2db1aefbd8377560e19b3e0d322180106fcf5
nahye03 <020977@naver.com>
2023-11-13 22:42:01 +0900

    Fixed draw_rectangle function

--- modules/LaneDetection/include/draw.hpp
+++ modules/LaneDetection/include/draw.hpp
@@ -20,12 +20,8 @@ namespace XyCar
         int32_t y2 = std::round(y1>>1);
         int32_t x1 = std::round((y1 - intercept) / slope);
         int32_t x2 = std::round((y2 - intercept) / slope);
-        // std::cout << "x1: " << x1 <<std::endl;
-        // std::cout << "x2: " << x2 <<std::endl;
 
         cv::line(frame, cv::Point(x1, y1), cv::Point(x2, y2), color, 2, cv::LINE_8);
-        // cv::imshow("frame", frame);
-
     }
 
     /* @details  Draw a line using Coordination on 'frame'.
@@ -47,7 +43,7 @@ namespace XyCar
     */
     void draw_rectangle(cv::Mat& frame, int32_t pos, const cv::Scalar& color)
     {
-        cv::rectangle(frame, cv::Rect(cv::Point(pos - 5, k_offset - 5), cv::Point(pos + 5, k_offset - 5)), color, 2);
+        cv::rectangle(frame, cv::Rect(cv::Point(pos - 7, k_offset - 7), cv::Point(pos + 7, k_offset + 7)), color, 2);
     }
 
 } // XyCar
