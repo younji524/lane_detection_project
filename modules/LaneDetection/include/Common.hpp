b0ae934feaaa48ce02a50713045eaca05fd87caf
dongwookheo <hdwook3918@gmail.com>
2023-11-15 00:14:35 +0900

    Added doxygen and some comments

--- modules/LaneDetection/include/Common.hpp
+++ modules/LaneDetection/include/Common.hpp
@@ -1,7 +1,9 @@
 #ifndef LANE_DETECTION__COMMON_HPP
 #define LANE_DETECTION__COMMON_HPP
 
+// System header
 #include <cstdint>
+// Third party header
 #include <yaml-cpp/yaml.h>
 
 namespace XyCar
@@ -13,13 +15,13 @@ namespace XyCar
      */
     struct State
     {
-        PREC left_slope_;
-        PREC left_intercept_;
-        PREC right_slope_;
-        PREC right_intercept_;
-        PREC left_pos_;
-        PREC right_pos_;
-        bool stop_flag_;
+        PREC left_slope_;  ///< The slope of left lane.
+        PREC left_intercept_;  ///< The intercept of left lane.
+        PREC right_slope_;  ///< The slope of right lane.
+        PREC right_intercept_;  ///< The intercept of right lane.
+        PREC left_pos_;  ///< The position of left lane.
+        PREC right_pos_;  ///< The position of right lane.
+        bool stop_flag_;  ///< The flag for stop line recognition.
     };
 } // XyCar
 
