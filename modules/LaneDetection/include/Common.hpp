ed3b24e547442fba6f7f4e774976ebb1ea40d179
dongwookheo <hdwook3918@gmail.com>
2023-11-15 00:29:52 +0900

    Edited indenting

--- modules/LaneDetection/include/Common.hpp
+++ modules/LaneDetection/include/Common.hpp
@@ -8,21 +8,20 @@
 
 namespace XyCar
 {
-    using PREC = double;  ///< PRECISION Data type for maintenance.
+using PREC = double; ///< PRECISION Data type for maintenance.
 
-    /**
-     * @details  A structure that represents the state of the lanes.
-     */
-    struct State
-    {
-        PREC left_slope_;  ///< The slope of left lane.
-        PREC left_intercept_;  ///< The intercept of left lane.
-        PREC right_slope_;  ///< The slope of right lane.
-        PREC right_intercept_;  ///< The intercept of right lane.
-        PREC left_pos_;  ///< The position of left lane.
-        PREC right_pos_;  ///< The position of right lane.
-        bool stop_flag_;  ///< The flag for stop line recognition.
-    };
-} // XyCar
+/**
+ * @details  A structure that represents the state of the lanes.
+ */
+struct State {
+  PREC left_slope_;      ///< The slope of left lane.
+  PREC left_intercept_;  ///< The intercept of left lane.
+  PREC right_slope_;     ///< The slope of right lane.
+  PREC right_intercept_; ///< The intercept of right lane.
+  PREC left_pos_;        ///< The position of left lane.
+  PREC right_pos_;       ///< The position of right lane.
+  bool stop_flag_;       ///< The flag for stop line recognition.
+};
+} // namespace XyCar
 
 #endif // LANE_DETECTION__COMMON_HPP
