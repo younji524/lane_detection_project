6456e558d9cc215ced0f7f8a4f6996747b327557
dongwookheo <hdwook3918@gmail.com>
2023-11-26 00:32:34 +0900

    Fixed doxygen

--- modules/LaneDetection/include/ImageProcessor.hpp
+++ modules/LaneDetection/include/ImageProcessor.hpp
@@ -3,7 +3,6 @@
 
 // Third party header
 #include "opencv2/opencv.hpp"
-
 // User defined header
 #include "Common.hpp"
 
@@ -18,32 +17,32 @@ public:
   using Ptr = ImageProcessor *; ///< Pointer type of class.
 
   /**
-   * @details  Construct a new Image Processor object
-   * @param[in]  config  The configuration of lane_detection project.
+   * @details Construct a new Image Processor object
+   * @param[in] config The configuration of lane_detection project.
    */
   ImageProcessor(const YAML::Node &config) { set_configuration(config); }
 
   /**
-   * @details  Perform the image preprocessing tasks below.
+   * @details Perform the image preprocessing tasks below.
    * 1. Crop the image for ROI settings.
    * 2. Convert to grayscale.
    * 3. Perform a Gaussian Blur.
    * 4. Apply a Canny Algorithm.
-   * @param[in]  frame  The original video frame.
-   * @return  cv::Mat
+   * @param[in] frame The original video frame.
+   * @return cv::Mat
    */
   cv::Mat process(const cv::Mat &frame);
 
 private:
-  cv::Mat cropped_frame_;    ///< The cropped image frame for lane detection.
+  cv::Mat cropped_frame_;     ///< The cropped image frame for lane detection.
   uint32_t roi_frame_y_;      ///< The y-coordinate for roi setting.
   uint32_t frame_width_;      ///< The frame width of an original image.
   uint32_t roi_frame_height_; ///< The height of frame for roi setting.
 
   /**
-   * @details  Set values from configuration.
-   * @param[in]  config  The configuration of lane_detection project.
-   * @return  void
+   * @details Set values from configuration.
+   * @param[in] config The configuration of lane_detection project.
+   * @return void
    */
   void set_configuration(const YAML::Node &config);
 };
