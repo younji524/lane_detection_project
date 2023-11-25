b9b7947a44013ecbda246bcd459fcc5b7f1ea804
dongwookheo <hdwook3918@gmail.com>
2023-11-26 00:56:10 +0900

    Fixed doxygen and added transpose matrix variables

--- modules/LaneDetection/include/KalmanFilter.hpp
+++ modules/LaneDetection/include/KalmanFilter.hpp
@@ -3,7 +3,6 @@
 
 // Third party header
 #include "opencv2/opencv.hpp"
-
 // User defined header
 #include "Common.hpp"
 
@@ -15,7 +14,12 @@ namespace XyCar
 class KalmanFilter
 {
 public:
-  using Ptr = KalmanFilter *;
+  using Ptr = KalmanFilter *;  ///< Pointer type of class.
+
+  /**
+   * @details Construct a new Kalman Filter object
+   * @param[in] config The configuration of lane_detection project.
+   */
   KalmanFilter(const YAML::Node &config);
 
   void kalman_filtering(PREC slope, PREC intercept);
@@ -35,13 +39,15 @@ private:
   PREC dt_;
   bool is_first_ = true;
 
-  cv::Mat_<PREC> state_matrix_; ///< x: The State estimates (상태 추정치)
-  cv::Mat_<PREC> transition_matrix_; ///< A: The State Transistion Matrix (상태 변환 행렬)
-  cv::Mat_<PREC> measurement_matrix_; ///< H: The Measurement Matrix (측정 행렬)
-  cv::Mat_<PREC> process_noise_matrix_; ///< Q: The Process Noise Covariance Matrix (과정 노이즈 공분산)
+  cv::Mat_<PREC> state_matrix_;             ///< x: The State estimates (상태 추정치)
+  cv::Mat_<PREC> transition_matrix_;        ///< A: The State Transition Matrix (상태 변환 행렬)
+  cv::Mat_<PREC> transition_matrix_t_;      ///< A^T: The transpose matrix of State Transition Matrix
+  cv::Mat_<PREC> measurement_matrix_;       ///< H: The Measurement Matrix (측정 행렬)
+  cv::Mat_<PREC> measurement_matrix_t_;     ///< H^T: The transpose matrix of Measurement Matrix
+  cv::Mat_<PREC> process_noise_matrix_;     ///< Q: The Process Noise Covariance Matrix (과정 노이즈 공분산)
   cv::Mat_<PREC> measurement_noise_matrix_; ///< R: The Measurement Noise Covariance Matrix (측정 노이즈 공분산)
-  cv::Mat_<PREC> covariance_matrix_; ///< P: The Error Covariance matrix (오차 공분산)
-  cv::Mat_<PREC> kalman_gain_; ///< K: The Kalman gain (칼만 이득)
+  cv::Mat_<PREC> covariance_matrix_;        ///< P: The Error Covariance matrix (오차 공분산)
+  cv::Mat_<PREC> kalman_gain_;              ///< K: The Kalman gain (칼만 이득)
 
   /**
    * @details Perform the prediction steps of the Kalman filter.
@@ -56,8 +62,8 @@ private:
   /**
    * @details Update the internal state of a Kalman filter given the 'average slope', 'average intercept' and 'position of lane'.
               The Kalman gain is computed, and the state matrix and covariance matrix are updated accordingly.
-              If the position is less than or equal to 0, or greater than or equal to 640,
-              the state matrix is updated with a measurement matrix derived from the average slope and intercept.
+              If the slope is not equal to 0, or the intercept is not equal to 0,
+              the state matrix is updated with a measurement derived from the average slope and intercept.
    * @param[in] avg_slope The average slope of the lane.
    * @param[in] avg_intercept The intercept slope of the lane.
    * @return void
