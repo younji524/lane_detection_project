b479472167b2bef6e3d235780d9e794f81f08344
dongwookheo <hdwook3918@gmail.com>
2023-11-28 22:38:39 +0900

    Edited doxygen

--- modules/LaneDetection/include/Common.hpp
+++ modules/LaneDetection/include/Common.hpp
@@ -1,17 +1,28 @@
+/**
+ * @file Common.hpp
+ * @author Nahye Kim (nahelove03@gmail.com) Dongwook Heo (hdwook3918@gmail.com)
+ * @brief Provides the definition of the lane state structure and precision type alias in the XyCar namespace.
+ * @version 1.0.0
+ * @date 2023-11-12
+ * @copyright Copyright (c) 2023 I_On_Car, All Rights Reserved.
+ */
 #ifndef LANE_DETECTION__COMMON_HPP
 #define LANE_DETECTION__COMMON_HPP
 
 // System header
 #include <cstdint>
 // Third party header
-#include <yaml-cpp/yaml.h>
+#include "yaml-cpp/yaml.h"
 
 namespace XyCar
 {
 using PREC = double; ///< PRECISION Data type for maintenance.
 
 /**
- * @details  A structure that represents the state of the lanes.
+ * @brief State struct that represents the state of the lanes.
+ * @details This structure is used to store information about the state of the lanes.
+ * State struct contains the slopes, intercept, positions of the left and right lanes
+ * and a flag for stop line recognition.
  */
 struct State
 {
